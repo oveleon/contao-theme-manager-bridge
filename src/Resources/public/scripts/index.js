a851c7eef530c017eda45b8cd247e110a9dfbb5a
doishub <daniele@oveleon.de>
2022-11-02 16:59:55 +0100

    Update js

--- src/Resources/public/scripts/index.js
+++ src/Resources/public/scripts/index.js
@@ -1,31 +1,46 @@
 import tingle from "tingle.js"
 import "tingle.js/dist/tingle.css"
 
+/**
+ * Import content package
+ */
 var importModal = new tingle.modal({
     footer: true,
     stickyFooter: false,
     closeMethods: ['escape'],
     cssClass: ['theme-assistant-modal']
-});
+})
 
-importModal.addFooterBtn('Abbrechen', 'tl_submit', function() {
+importModal.addFooterBtn('Abbrechen', 'tl_submit', () => {
     importModal.close()
-});
+})
 
-importModal.addFooterBtn('Importieren', 'tl_submit', function() {
-    importModal.close()
-});
+importModal.addFooterBtn('Importieren', 'tl_submit', () => {
+    const form = importModal.modalBoxContent.querySelector('form')
+
+    if(!form.checkValidity())
+    {
+        form.reportValidity()
+        return
+    }
+
+    form.submit()
+    exportModal.close()
+})
 
 for(let button of document.querySelectorAll('.assistant-module a.import'))
 {
     button.addEventListener('click', (e) => {
         e.preventDefault()
 
-        importModal.setContent('Theme importing is currently under development.')
+        importModal.setContent(getTemplate('#' + button.dataset.template))
         importModal.open()
     })
 }
 
+/**
+ * Export content package
+ */
 var exportModal = new tingle.modal({
     footer: true,
     stickyFooter: false,
@@ -33,34 +48,30 @@ var exportModal = new tingle.modal({
     cssClass: ['theme-assistant-modal']
 });
 
-exportModal.addFooterBtn('Abbrechen', 'tl_submit', function() {
+exportModal.addFooterBtn('Abbrechen', 'tl_submit', () => {
     exportModal.close()
 });
 
-exportModal.addFooterBtn('Exportieren', 'tl_submit', function() {
-    const form = exportModal.modalBoxContent.querySelector('form');
+exportModal.addFooterBtn('Exportieren', 'tl_submit', () => {
+    const form = exportModal.modalBoxContent.querySelector('form')
 
     if(!form.checkValidity())
     {
         form.reportValidity()
-        return;
+        return
     }
 
     form.submit()
     exportModal.close()
-});
+})
 
 document.querySelector('#theme-assistant-buttons a.content-export').addEventListener('click', (e) => {
     e.preventDefault()
 
-    exportModal.setContent(createExportTemplate())
+    exportModal.setContent(getTemplate("#content-export-template"))
     exportModal.open()
 })
 
-function createExportTemplate(){
-    const template = document.querySelector("#content-export-template").content.cloneNode(true)
-
-    //
-
-    return template
+function getTemplate(selector){
+    return document.querySelector(selector).content.cloneNode(true)
 }
